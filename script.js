163e304300d065d0ae4d96c4369d0de75840242b
TWICEChewy <47435103+TWICEChewy@users.noreply.github.com>
2019-05-07 14:45:54 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,23 @@
+$("#gnt").click(function(){
+    $("#gnt img").attr("src","https://vignette.wikia.nocookie.net/callofduty/images/f/ff/115_Generators_all_active_BOII.png/revision/latest?cb=20130928001449");
+});
+$("#gnt").click(function(){
+    $("#gnt img").attr("src","https://vignette.wikia.nocookie.net/callofduty/images/f/ff/115_Generators_all_active_BOII.png/revision/latest?cb=20130928001449");
+    $("h1").text("Upgrade the Elemental Staffs");
+});
+$("#fire").click(function(){
+    $("#fire").attr("src","https://pbs.twimg.com/media/C_aTCXsXcAAO61o.png");
+}); 
+$("#lightning").click(function(){
+    $("#lightning").attr("src","https://pbs.twimg.com/media/C_aTCVzXsAIONga.png");
+}); 
+$("#ice").click(function(){
+    $("#ice").attr("src","https://pbs.twimg.com/media/C_aTCVrXkAAUuGY.png");
+}); 
+$("#wind").click(function(){
+    $("#wind").attr("src","https://pbs.twimg.com/media/C_aTCVuXcAESyQk.png");
+    $("img").delay(700).fadeOut();
+    $("p").hide();
+    $("h1").hide();
+});  
+
